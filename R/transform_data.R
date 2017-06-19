d5012269bc9014dc30ad8c3d69bf0912a6b27f12
PNL\joha328 <thomas.johansen@pnnl.gov>
2017-06-19 16:34:36 -0700

    Add group_designation function from pmartRqc package. Add "zero" and "user" methods to transform_data. Test transformation methods in Rmd file.

--- R/transform_data.R
+++ R/transform_data.R
@@ -9,64 +9,88 @@
 #'
 #' @examples
 transform_data <- function(rRNA_obj, method = "clr", shift = 0.5, basis_ind = NULL,
-                           sampleMC = FALSE, MCdistn = "dirichlet", niter = 200) {
+                           sampleMC = FALSE, ...) {
+  .transform_data(rRNA_obj, method, shift, basis_ind, sampleMC, ...)
+}
+
+.transform_data <- function(rRNA_obj, method = "clr", shift = 0.5, basis_ind = NULL,
+                            sampleMC = FALSE, MCdistn = "dirichlet", niter = 200,
+                            include_zero_rows = FALSE, quiet = FALSE) {
 
   if (class(rRNA_obj) != "rRNAdata")
     stop("rRNA_obj must be of class 'rRNAdata.' See ?as.rRNAdata for more info.")
   if(is.null(method))
-    stop("method must be one of clr, alr, or iqlr")
-  if (!(tolower(method) %in% c("clr", "alr", "iqlr")))
-    stop("method must be one of clr, alr, or iqlr")
+    stop("method must be one of clr, alr, iqlr, zero, or user")
+  if (!(tolower(method) %in% c("clr", "alr", "iqlr", "zero", "user")))
+    stop("method must be one of clr, alr, iqlr, zero, or user")
   if (!(class(shift) %in% c("numeric", "integer")))
     stop("shift must be a number")
-  if (!is.null(basis_ind)) {
-    if (!(class(basis_ind) %in% c("numeric", "integer")))
-      stop("basis_ind must be a positive integer no greater than the number of rows in e_data")
-    if(basis_ind < 1 | basis_ind > nrow(rRNA_obj$e_data) | basis_ind %% 1 != 0)
-      stop("basis_ind must be a positive integer no greater than the number of rows in e_data")
-  }
 
 
   ret <- rRNA_obj
   transdata <- ret$e_data
-
-  # # Here a separate function will be called to get the shifted data
-  # transdata <- get_shift_fun(ret$e_data, shift)
-
-  # for now we'll do this...
   edata_cname <- attributes(rRNA_obj)$cnames$edata_cname
-  ind <- which(colnames(transdata) == edata_cname)
-  transdata[,-ind] <- log2(transdata[,-ind] + shift)
+  cind <- which(colnames(transdata) == edata_cname)
 
-  # # If user supplies a distribution, a function will be called to do the sampling
+  # remove any OTUs containing all zeros
+  if (!include_zero_rows) {
+    Not_zero <- transdata[,-cind] != 0
+    ind_allzero <- which(rowSums(Not_zero) == 0)
+    if (length(ind_allzero > 0)) {
+      if (tolower(method) %in% c("user", "alr")) {
+        if (any(basis_ind %in% ind_allzero))
+          stop("At least one of selected OTUs for basis_ind is all zeros")
+
+      }
+      transdata <- transdata[-ind_allzero, ]
+      if(!quiet) {
+        message(paste0("Removing the following rows containing all zeros:\n",
+                      paste(ind_allzero, collapse = ", ")))
+      }
+    }
+  }
+
+  # shift data
+  transdata[,-cind] <- transdata[,-cind] + shift
+
+
+  # # If user supplies a distribution a function will be called to do the sampling,
+  # # something like this...
   # if (sampleMC) {
   #   transdata <- get_MC_samples_fun(transdata, MCdistn, niter)
   # }
   # # probably do the log transformation at this point once the other functions are in
-  # transdata[,-ind] <- log2(transdata[,-ind])
+  # transdata[,-cind] <- log2(transdata[,-cind])
+
+  # log transform
+  transdata[,-cind] <- log2(transdata[,-cind])
 
 
   # CLR transformation
   if (tolower(method) == "clr") {
-    transdata[,-ind] <- apply(transdata[,-ind], 2, function(x){x - mean(x)})
-  }
+    transdata[,-cind] <- apply(transdata[,-cind], 2, function(x){x - mean(x)})
 
 
   # ALR transformation
-  if (tolower(method) == "alr") {
+  } else if (tolower(method) == "alr") {
+
+    # check for errors
+    if (is.null(basis_ind))
+      stop("Please supply an integer for basis_ind to specify which OTU will be used as the basis")
+    if (length(basis_ind) != 1)
+      stop("basis_ind must be of length 1 when 'alr' method is used")
+
     # use an OTU as a basis to scale the others
-    denom <- ifelse(is.null(basis_ind), nrow(transdata), basis_ind)
-    transdata[,-ind] <- apply(transdata[,-ind], 2, function(x){x - x[denom]})
+    transdata[,-cind] <- apply(transdata[,-cind], 2, function(x){x - x[basis_ind]})
 
     # OTU used for basis is taken out
-    transdata <- transdata[-denom,]
-  }
+    transdata <- transdata[-basis_ind,]
 
 
   # IQLR transformation
-  if (tolower(method) == "iqlr") {
+  } else if (tolower(method) == "iqlr") {
     # first run CLR
-    tempdata <- apply(transdata[,-ind], 2, function(x){x - mean(x)})
+    tempdata <- apply(transdata[,-cind], 2, function(x){x - mean(x)})
 
     # calculate variance of OTUs
     otu_var <- apply(tempdata, 1, var)
@@ -78,12 +102,68 @@ transform_data <- function(rRNA_obj, method = "clr", shift = 0.5, basis_ind = NU
     geom_ind <- which(otu_var >= q[1] & otu_var <= q[2])
 
     # get means
-    new_means <- colMeans(transdata[geom_ind, -ind])
+    new_means <- colMeans(transdata[geom_ind, -cind])
 
     # scale data
-    transdata[,-ind] <- transdata[,-ind] - matrix(new_means, nrow = nrow(transdata[,-1]),
+    transdata[,-cind] <- transdata[,-cind] - matrix(new_means, nrow = nrow(transdata),
                                               ncol = length(new_means), byrow = TRUE)
+
+
+  # Zero transformation
+  } else if (tolower(method) == "zero") {
+    if (is.null(attributes(ret)$group_DF))
+      stop("No group designated for rRNA_obj. See ?group_designation for details.")
+
+    # get group designation and sample ID
+    gdf <- attributes(ret)$group_DF
+    samp_id <- attributes(ret)$cname$fdata_cname
+
+    # define function for finding nonzero OTUs within each group and calculating sample means
+    nonzero_trans <- function(group) {
+
+      # subset by the group provided
+      temp <- subset(gdf, Group == group)
+      samps <- as.character(temp[, samp_id])
+
+      # find the rows within that group (of original data) that aren't zero
+      # ind <- which(rowSums(ret$e_data[, samps]) != 0)
+
+      if (length(ind_allzero) == 0) {
+        Not_zero <- ret$e_data[, samps] != 0
+      } else {
+        Not_zero <- ret$e_data[-ind_allzero, samps] != 0
+      }
+
+      # calculate the means of each sample with only the nonzero rows
+      cmeans <- colMeans(transdata[rowSums(Not_zero) > 0, samps])
+
+      return(cmeans)
+    }
+
+
+    # get all means and order them properly
+    temp_means <- unlist(lapply(unique(gdf$Group), nonzero_trans))
+    nm <- data.frame(Names = names(temp_means), Values = temp_means, row.names = NULL)
+    new_means <- nm$Values[match(colnames(transdata[,-cind]), nm$Names)]
+
+    # scale data
+    transdata[,-cind] <- transdata[,-cind] - matrix(new_means, nrow = nrow(transdata),
+                                                    ncol = length(new_means), byrow = TRUE)
+
+
+  # User transformation
+  } else if (tolower(method) == "user") {
+    if (is.null(basis_ind))
+      stop("basis_ind cannot be NULL for this method, please supply a vector of indices")
+
+    # get means
+    new_means <- colMeans(transdata[basis_ind, -cind])
+
+    # scale data
+    transdata[,-cind] <- transdata[,-cind] - matrix(new_means, nrow = nrow(transdata),
+                                                    ncol = length(new_means), byrow = TRUE)
   }
+
 
   ret$e_data <- transdata
 
