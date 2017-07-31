d2119ceddf236722711c1607665c50b04e1f9ace
PNL\joha328 <thomas.johansen@pnnl.gov>
2017-07-31 09:41:09 -0700

    Remove dependencies on Rcpp

--- R/imd_filter.R
+++ R/imd_filter.R
@@ -1,22 +1,22 @@
 #' Title
 #'
-#' @param rRNA_obj
+#' @param rRNAdata
 #'
 #' @return
 #' @export
 #'
 #' @examples
-nonmissing_filter <- function(rRNA_obj) {
+imd_filter <- function(rRNAdata) {
 
-  if (class(rRNA_obj) != "rRNAdata") stop("rRNA_obj must be of class 'rRNAdata'")
-  gdf <- attr(rRNA_obj, "group_DF")
+  if (class(rRNAdata) != "rRNAdata") stop("rRNAdata must be of class 'rRNAdata'")
+  gdf <- attr(rRNAdata, "group_DF")
   if (is.null(gdf)) stop("group_designation() must be run prior to nonmissing_filter()")
 
-  nonmiss_per_group <- nonmissing_per_group(rRNA_obj)
+  nonmiss_per_group <- nonmissing_per_group(rRNAdata)
   output <- nonmiss_per_group$nonmiss_totals
 
   orig_class <- class(output)
-  class(output) <- c("nonmissFilt", orig_class)
+  class(output) <- c("imdFilt", orig_class)
 
   attr(output, "group_sizes") <- nonmiss_per_group$group_sizes
 
